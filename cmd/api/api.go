8c5aab22f4f3174a5152f6e2b2a5388c74b48f1c
Chris Connor <chris.connor@comcarde.com>
2024-12-04 20:06:45 +0000

    Adding initial structure

---
+++ cmd/api/api.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) run() error {
+
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    app.config.addr,
+		Handler: mux,
+	}
+
+	log.Printf("server started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cjconnor24/social
+
+go 1.23.0
